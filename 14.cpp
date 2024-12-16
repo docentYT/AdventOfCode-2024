00b7d9b4ce023f384c396b68a305f3255701e5c6
docentYT <63965954+docentYT@users.noreply.github.com>
2024-12-16 14:52:26 +0100

    day 14 part 2

--- 14.cpp
+++ 14.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-constexpr int TIMES = 100;
 constexpr int ROWS = 103;
 constexpr int COLUMNS = 101;
 
-void predict_robot_position(pair<int, int>& position,
-                            const pair<int, int>& velocity) {
-    position.first = (position.first + velocity.first * TIMES) % ROWS;
-    if (position.first < 0) {
-        position.first = ROWS + position.first;
+struct Robot {
+    pair<int, int> pos, vel;
+};
+
+Robot parse_input_line(string& line) {
+    Robot robot;
+    stringstream ss(line);
+    ss.ignore(2);
+    ss >> robot.pos.second;
+    ss.ignore(1);
+    ss >> robot.pos.first;
+    ss.ignore(3);
+    ss >> robot.vel.second;
+    ss.ignore(1);
+    ss >> robot.vel.first;
+    return robot;
+}
+
+void predict_robot_position(Robot& robot, int seconds) {
+    robot.pos.first = (robot.pos.first + robot.vel.first * seconds) % ROWS;
+    if (robot.pos.first < 0) {
+        robot.pos.first = ROWS + robot.pos.first;
     }
-    position.second = (position.second + velocity.second * TIMES) % COLUMNS;
-    if (position.second < 0) {
-        position.second = COLUMNS + position.second;
+    robot.pos.second =
+        (robot.pos.second + robot.vel.second * seconds) % COLUMNS;
+    if (robot.pos.second < 0) {
+        robot.pos.second = COLUMNS + robot.pos.second;
     }
 }
 
-int main() {
+void part_one() {
     int map[ROWS][COLUMNS]{};
 
     string line;
-    pair<int, int> robot_position;
-    pair<int, int> robot_velocity;
     while (getline(cin, line)) {
-        stringstream ss(line);
-        ss.ignore(2);
-        ss >> robot_position.second;
-        ss.ignore(1);
-        ss >> robot_position.first;
-        ss.ignore(3);
-        ss >> robot_velocity.second;
-        ss.ignore(1);
-        ss >> robot_velocity.first;
+        auto robot = parse_input_line(line);
 
-        predict_robot_position(robot_position, robot_velocity);
-        map[robot_position.first][robot_position.second]++;
+        predict_robot_position(robot, 100);
+        map[robot.pos.first][robot.pos.second]++;
     }
 
     int quadrant_TL = 0;
@@ -63,6 +71,48 @@ int main() {
     }
 
     cout << quadrant_TL * quadrant_TR * quadrant_BL * quadrant_BR;
+}
+
+bool is_easter_egg(int map[ROWS][COLUMNS]) {
+    // When none of the robots are in the same place (works for my input)
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            if (map[i][j] >= 2) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void part_two() {
+    int map[ROWS][COLUMNS]{};
+    vector<Robot> robots;
+
+    string line;
+    while (getline(cin, line)) {
+        robots.push_back(parse_input_line(line));
+    }
+
+    int seconds = 1;
+    while (!is_easter_egg(map) || seconds == 1) {  // Firstly the map is empty
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                map[i][j] = 0;
+            }
+        }
+        for (auto robot : robots) {
+            predict_robot_position(robot, seconds);
+            map[robot.pos.first][robot.pos.second]++;
+        }
+        seconds++;
+    }
+    cout << seconds - 1;
+}
+
+int main() {
+    part_one();
+    // part_two();
 
     return 0;
 }
